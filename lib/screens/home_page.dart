15e5806090043b0e8af3f6d66452898d06ce197f
Yashu-Chaudhary <yashusky232@gmail.com>
2024-05-22 07:41:44 +0530

    resistration

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,27 +8,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.red,));
+      statusBarColor: Colors.blue,));
     return   Scaffold(
       
       body: Column(
         children: [
       const SizedBox(height: 22),
           Container(
+            height: 80,
             decoration: const BoxDecoration(color: PColors.primary),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // logo
-                  IconButton(onPressed: (){}, icon:const Icon(Icons.list, size: 32,))
-                  // Name
-                      
-                  // Language selection
+                  // nav
+                  IconButton(onPressed: (){}, icon:const Icon(Icons.list, size: 30,color: Colors.white,))
+                  
                 ],
               ),
             ),
-          )
+          ),
+
+          // container for the logo name lang resiter
+          Container(
+            width: MediaQuery.of(context).size.width ,
+            // height: 70,
+            decoration: const BoxDecoration(color: Colors.red),
+            child: const Column(
+              children: [
+                Row(
+                  children: [
+                    // logo
+                    
+                    // Name
+
+                    // lang
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       )
     );
